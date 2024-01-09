474cb2232914da38a2e3e382d3115a9c714f0d93
Rumman Parvez <116460823+rummanparvez@users.noreply.github.com>
2024-01-10 00:37:20 +0530

    Add files via upload

---
+++ Diseases_Prediction.py
@@ -0,0 +1,184 @@
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+diabetes_mod=pickle.load(open('C:/Users/coolr/Desktop/Diseases Prediction System/saved_models/diabetes_model.sav','rb'))
+
+heart_disease_mod=pickle.load(open('C:/Users/coolr/Desktop/Diseases Prediction System/saved_models/heart_disease_model.sav','rb'))
+
+parkinsons_mod=pickle.load(open('C:/Users/coolr/Desktop/Diseases Prediction System/saved_models/parkinsons_model.sav','rb'))
+
+
+with st.sidebar:
+    select=option_menu("Multiple Disease Prediction System",
+                         ['Diabetes Prediction',
+                          'Heart Disease Prediction',
+                          'Parkinsons Prediction'],
+                         icons=['droplet','activity','person'],
+                         default_index=0)
+
+#Diabetes Prediction
+if(select=='Diabetes Prediction'):
+    st.title('Diabetes Prediction')
+    
+    col1,col2,col3=st.columns(3)
+    
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure Value')
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness Value')
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+    with col3:
+        BMI = st.text_input('BMI Value')
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function Value')
+    with col2:
+        Age = st.text_input('Age of the Person')
+
+    #Prediction
+    diabetes_diagnosis=''
+    
+    if st.button('Check Result'):
+        diabetes_pred=diabetes_mod.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+        
+        if(diabetes_pred[0]==1):
+            diabetes_diagnosis='The Person Is Diabetic'
+        else:
+            diabetes_diagnosis='The Person Is Not Diabetic'
+        
+    st.success(diabetes_diagnosis)
+    
+    
+
+#Heart Disease Prediction    
+if(select=='Heart Disease Prediction'):
+    st.title('Heart Disease Prediction')
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        age = st.text_input('Age')
+    with col2:
+        sex = st.text_input('Sex')
+    with col3:
+        cp = st.text_input('Chest Pain types')
+    with col1:
+        trestbps = st.text_input('Resting Blood Pressure')
+    with col2:
+        chol = st.text_input('Serum Cholestoral in mg/dl')
+    with col3:
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+    with col1:
+        restecg = st.text_input('Resting Electrocardiographic results')
+    with col2:
+        thalach = st.text_input('Maximum Heart Rate achieved')
+    with col3:
+        exang = st.text_input('Exercise Induced Angina')
+    with col1:
+        oldpeak = st.text_input('ST depression induced by exercise')
+    with col2:
+        slope = st.text_input('Slope of the peak exercise ST segment')
+    with col3:
+        ca = st.text_input('Major vessels colored by flourosopy')
+    with col1:
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+
+    # code for Prediction
+    heart_diagnosis = ''
+
+    # creating a button for Prediction
+
+    if st.button('Heart Disease Test Result'):
+
+        y = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+        y = [float(x) for x in user_input]
+
+        heart_pred = heart_disease_model.predict([y])
+
+        if heart_prediction[0] == 1:
+            heart_diagnosis = 'The person is having heart disease'
+        else:
+            heart_diagnosis = 'The person does not have any heart disease'
+
+    st.success(heart_diagnosis)
+    
+    
+    
+    
+
+    
+if(select=='Parkinsons Prediction'):
+    st.title('Parkinsons Prediction')
+    
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        fo = st.text_input('MDVP:Fo(Hz)')
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
+    with col3:
+        flo = st.text_input('MDVP:Flo(Hz)')
+    with col4:
+        Jitter_percent = st.text_input('MDVP:Jitter(%)')
+    with col5:
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+    with col1:
+        RAP = st.text_input('MDVP:RAP')
+    with col2:
+        PPQ = st.text_input('MDVP:PPQ')
+    with col3:
+        DDP = st.text_input('Jitter:DDP')
+    with col4:
+        Shimmer = st.text_input('MDVP:Shimmer')
+    with col5:
+        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+    with col1:
+        APQ3 = st.text_input('Shimmer:APQ3')
+    with col2:
+        APQ5 = st.text_input('Shimmer:APQ5')
+    with col3:
+        APQ = st.text_input('MDVP:APQ')
+    with col4:
+        DDA = st.text_input('Shimmer:DDA')
+    with col5:
+        NHR = st.text_input('NHR')
+    with col1:
+        HNR = st.text_input('HNR')
+    with col2:
+        RPDE = st.text_input('RPDE')
+    with col3:
+        DFA = st.text_input('DFA')
+    with col4:
+        spread1 = st.text_input('spread1')
+    with col5:
+        spread2 = st.text_input('spread2')
+    with col1:
+        D2 = st.text_input('D2')
+    with col2:
+        PPE = st.text_input('PPE')
+
+    parkinsons_diagnosis = ''
+
+    # creating a button for Prediction    
+    if st.button("Parkinson's Test Result"):
+
+        z = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
+                      RAP, PPQ, DDP,Shimmer, Shimmer_dB, APQ3, APQ5,
+                      APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
+        z = [float(x) for x in user_input]
+
+        parkinsons_prediction = parkinsons_model.predict([z])
+
+        if parkinsons_prediction[0] == 1:
+            parkinsons_diagnosis = "The person has Parkinson's disease"
+        else:
+            parkinsons_diagnosis = "The person does not have Parkinson's disease"
+
+    st.success(parkinsons_diagnosis)
+    
+
